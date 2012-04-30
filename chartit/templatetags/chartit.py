0cb85821f56f5c13a19fd96fba1a5ea783cd1f54
Trevor Joynson (trevorj) <github@skywww.net>
2012-04-30 01:11:42 -0400

    Change from django.utils.simplejson to python json module as the former is deprecated. Remove use_decimal kwarg since it's not supported.

--- chartit/templatetags/chartit.py
+++ chartit/templatetags/chartit.py
@@ -1,10 +1,10 @@
 from itertools import izip_longest
 
 from django import template
-from django.utils import simplejson
 from django.utils.safestring import mark_safe
 from django.conf import settings
 import posixpath
+import json
 
 from ..charts import Chart, PivotChart
 
@@ -66,8 +66,7 @@ def load_charts(chart_list=None, render_to=''):
         for hco, render_to in izip_longest(chart_list, render_to_list):
             if render_to:
                 hco['chart']['renderTo'] = render_to
-        embed_script = (embed_script % (simplejson.dumps(chart_list, 
-                                                         use_decimal=True),
+        embed_script = (embed_script % (json.dumps(chart_list),
                                         CHART_LOADER_URL))
     else:
         embed_script = embed_script %((), CHART_LOADER_URL)
